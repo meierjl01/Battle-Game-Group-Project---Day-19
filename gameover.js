10af30bf91dbcfbf9c97473d295af4ae933679f1
Jennifer Meier <meierjl01@gmail.com>
2016-10-20 15:30:21 -0500

    added gameover page

---
+++ gameover.js
@@ -0,0 +1,26 @@
+var header = $('header');
+var main = $('main');
+
+//locationhash will actually go at the bottom of the battle page's function
+location.hash="#gameover";
+
+console.log('hi');
+
+function renderGameOver() {
+    header.empty();
+    main.empty();
+    var head = $('<h1 class="game-over-header">Game Over!</h1>');
+    //need to include code that says who won in header also
+
+    //include gif or pic (both?) for each possible winner
+    var winner = $('<div class ="game-over-gif"><img src="pics/atleastyoutried.gif" /></div>');
+
+    header.append(head);
+    main.append(winner);
+
+    setTimeout(function() {
+        location.hash = '';
+    }, 10000);
+}
+
+renderGameOver();
