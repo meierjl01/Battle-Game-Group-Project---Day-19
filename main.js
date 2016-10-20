05d493c08a4ce2532ad9ed40161bc26b7fe6002f
Drake Fish <Drake_fish@yahoo.com>
2016-10-20 14:23:05 -0500

    built characters and added hashchange events

---
+++ main.js
@@ -0,0 +1,41 @@
+function Character(name){
+  'use strict';
+  this.name=name;
+  this.health=10;
+}
+var Lisa= new Character('Lisa');
+var Bart= new Character('Bart');
+var Homer= new Character('Homer');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function hashChangeEvent(e){
+  var siteLocation=location.hash;
+
+  if(siteLocation===''){
+    console.log("something");
+  } else if(siteLocation==='#battle'){
+    console.log("battle");
+  }else if(siteLocatin==='#gameover'){
+    console.log("yousuck!");
+  }
+}
+
+$(window).on('hashchange',hashChangeEvent);
+hashChangeEvent();
