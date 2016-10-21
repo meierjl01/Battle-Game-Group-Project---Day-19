7a3cc26e59abd1752f83d589e05b7bfea428b5d9
Jeff Lawlis <jeff.lawlis@yahoo.com>
2016-10-21 09:33:09 -0500

    added empty and specified character in battle

--- battle.js
+++ battle.js
@@ -1,20 +1,25 @@
 var main = $('main');
-function battleRender () {
+var header = $('header');
+var screenDiv = $('.screen');
+function battleRender (character) {
+  main.empty();
+  header.empty();
+  screenDiv.empty();
   //change based on character user selected
   var userDiv = $('<div class="userChar">'+
 //health will change when opponent attacks
-'<h3>Health</h3>'+
-'<h2>Name</h2>'+
-'<img src="pics/bart.png"/>'+
+'<h3>'+character.health+'</h3>'+
+'<h2>'+character.name+'</h2>'+
+'<img src="'+character.picture+'"/>'+
 '<button class="attack">Choke</button>'+
 '<button class="block">Block</button>'+
 //change innerText for super based on character
-'<button class="super">Super</button></div>');
+'<button class="super">'+character.super+'</button></div>');
 
   var compDiv = $('<div class="compChar">'+
   //health change when user attacks
 '<h3>Health</h3>'+
-'<h2>Name</h2>'+
+'<h2>Homer</h2>'+
 '<img src="pics/homer.png"/>'+
 '</div>');
 main.append(userDiv, compDiv);
