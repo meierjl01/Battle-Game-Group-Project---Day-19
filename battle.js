34c2a00e72d5b80ba6094707c970a0411d2bb360
Jeff Lawlis <jeff.lawlis@yahoo.com>
2016-10-23 14:08:46 -0500

    saving

--- battle.js
+++ battle.js
@@ -6,7 +6,7 @@ function battleRender(character) {
     character.health = 7;
     Homer.health = 7;
     screenDiv.empty();
-    body.css('background-image', 'url(pics/springfield.jpg)');
+    screenDiv.css('background-image', 'url(pics/springfield.jpg)');
     //change based on character user selected
     var userDiv = $('<div class="userChar">' +
         //health will change when opponent attacks
--- homepage.js
+++ homepage.js
@@ -1,6 +1,4 @@
 var screenDiv = $('.screen');
-var body = $('body');
-var title=$('<h1 class="home-title">THE ULTIMATE SIMPSONS FIGHTING GAME</h1><h2>Choose Your Character!</h2>');
 var title=$('<img class="home-title" src="pics/simpsonlogo.png"><img class="fight" src="pics/fight.png"><h2>Choose Your Character!</h2>');
 var lisaCharacter=$('<div class="character lisa"><img src="pics/Lisa.png"><p>Lisa Simpson is a ruthless character and has a huge thirst for blood. She is top of her class and #1 in kickin ass! She plays a mean sax and has the brains to defeat any character in her way</p></div>');
 var bartCharacter=$('<div class="character bart"><img src="pics/bart.png"><p>Bart Simpson is a rad dude. He has a ton of friends and is the coolest kid around. Hes got a blackbelt in karate and pranks like no other. This character is a force to be reckoned with.</p></div>');
@@ -11,7 +9,6 @@ var barthomeSound = new Audio('pics/bart_laughs.wav');
 
 function renderHomepage(){
 homeSound.play();
-body.css('background-image', 'url(pics/broketv.gif)');
 screenDiv.css('background-image', 'url(pics/broketv.gif)');
 screenDiv.append(title);
 screenDiv.append(lisaCharacter,bartCharacter,homerCharacter);
